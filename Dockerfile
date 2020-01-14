6d1feafbda276372c9fc186ab010c46d902487da
A-Team <ateam@pluralsight.com>
2020-01-14 17:13:52 -0500

    Add todo APIs
    Co-Authored-By: Andy Lincoln <andy@andylincoln.io>
    Co-Authored-By: Vishal Ramesh <vishalramesh50@users.noreply.github.com>
    Co-Authored-By: m-labbe <m-labbe@users.noreply.github.com>
    Co-Authored-By: willmcmain <willmcmain@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM python:3.7-slim-buster
 WORKDIR /opt/todo
 RUN apt-get update
 RUN apt-get install -y build-essential
+RUN python -m pip install poetry
 COPY pyproject.toml ./
 COPY poetry.lock ./
-RUN python -m pip install poetry
 RUN poetry install
 ENV FLASK_DEBUG 1
 ENV FLASK_APP server/app.py
--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template
+from flask.views import MethodView
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema, fields
 
 app = Flask(__name__, static_folder="../static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:example@database:5432/todo'
@@ -14,6 +16,44 @@ class TodoItem(db.Model):
     def __repr__(self):
         return f'<TodoItem {self.id} {self.text} {self.completed}>'
 
+
+class TodoItemSchema(Schema):
+    id = fields.Integer()
+    text = fields.String()
+    completed = fields.Boolean()
+
+
+class TodoAPI(MethodView):
+    def get(self, todo_item_id):
+        if todo_item_id is None:
+            items = TodoItem.query.all()
+            schema = TodoItemSchema(many=True)
+            return {'status': 'success',
+                    'data': schema.dump(items)}
+        else:
+           item = TodoItem.query.filter_by(id=todo_item_id).first()
+           schema = TodoItemSchema()
+           return {'status': 'success',
+                    'data': schema.dump(item)}
+
+    def post(self):
+        # create a new user
+        pass
+
+    def delete(self, todo_item_id):
+        # delete a single user
+        pass
+
+    def put(self, todo_item_id):
+        # update a single user
+        pass
+
+todo_item_view = TodoAPI.as_view('todo_api')
+app.add_url_rule('/todo/', defaults={'todo_item_id': None},
+                 view_func=todo_item_view, methods=['GET',])
+app.add_url_rule('/todo/', view_func=todo_item_view, methods=['POST',])
+app.add_url_rule('/todo/<int:todo_item_id>', view_func=todo_item_view,
+                 methods=['GET', 'PUT', 'DELETE'])
 
 
 @app.route('/')
