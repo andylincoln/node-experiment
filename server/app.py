05d1d5e3ef09e77b7152dce672331e7a516b0429
A-Team <ateam@pluralsight.com>
2020-01-14 16:40:50 -0500

    Add Postgres connection to app via flask-sqlalchemy
    Co-Authored-By: Andy Lincoln <andy@andylincoln.io>
    Co-Authored-By: Vishal Ramesh <vishalramesh50@users.noreply.github.com>
    Co-Authored-By: m-labbe <m-labbe@users.noreply.github.com>
    Co-Authored-By: willmcmain <willmcmain@users.noreply.github.com>

--- server/app.py
+++ server/app.py
@@ -1,7 +1,20 @@
 import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder="../static")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:example@database:5432/todo'
+db = SQLAlchemy(app)
+
+class TodoItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    text = db.Column(db.Text, nullable=False, default="")
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f'<TodoItem {self.id} {self.text} {self.completed}>'
+
+
 
 @app.route('/')
 def hello():
