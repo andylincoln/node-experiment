540a2a16358dc2c74c5eb9ac2220e3b6b7f13b9a
A-Team <ateam@pluralsight.com>
2020-01-15 13:14:05 -0500

    Add api tests
    Co-Authored-By: Andy Lincoln <andy@andylincoln.io>
    Co-Authored-By: m-labbe <m-labbe@users.noreply.github.com>
    Co-Authored-By: willmcmain <willmcmain@users.noreply.github.com>

--- server/test_app.py
+++ server/test_app.py
@@ -21,3 +21,25 @@ def test_get_all(client):
         ]
     }
     assert actual == expected
+
+def test_add_item(client):
+    client.post('/todo/', json={"completed": False, "text": "Add new item"})
+    actual = TodoItem.query.filter(TodoItem.text == "Add new item").one()
+    assert actual.text == "Add new item"
+    assert actual.completed == False
+
+def test_delete_item(client):
+    delete_me = TodoItem(id=1, completed=False, text="Delete this item")
+    db.session.add(delete_me)
+    db.session.flush()
+    client.delete('/todo/1/')
+    actual = TodoItem.query.filter_by(id=1).first()
+    assert actual is None
+
+def test_update_item(client):
+    item = TodoItem(id=1, completed=False, text="Test item three text")
+    db.session.add(item)
+    db.session.flush()
+    client.put('/todo/1/', json={"completed": True})
+    actual = TodoItem.query.filter_by(id=1).one()
+    assert actual.completed
