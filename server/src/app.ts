fdc6f6c6951f6f36867ea99d204cb1020fef35d9
A-Team <ateam@pluralsight.com>
2020-01-16 13:42:31 -0500

    switch to TypeORM
    Co-Authored-By: m-labbe <m-labbe@users.noreply.github.com>
    Co-Authored-By: willmcmain <willmcmain@users.noreply.github.com>
    Co-Authored-By: Vishal Ramesh <vishalramesh50@users.noreply.github.com>
    Co-Authored-By: alancinacio <alancinacio@users.noreply.github.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,14 @@
+import 'reflect-metadata'
+import { createConnection, Connection } from 'typeorm'
 import express from 'express'
-import { Model } from 'objection'
-import Knex from 'knex'
-import knexConfig from './knexfile'
 
 const app = express()
-
-app.get('/', (req, res) => {
-  res.send('I love it when a plan comes together!')
+createConnection().then((connection) => {
+  if (connection) {
+    app.get('/', (req, res) => {
+      res.send('I love it when a plan comes together!')
+    })
+  }
 })
-
-// Initialize knex.
-const knex = Knex(knexConfig.development)
-
-// Give the knex instance to objection.
-Model.knex(knex)
 
 export default app
