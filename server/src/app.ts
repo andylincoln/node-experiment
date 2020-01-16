ca632584a688b4695b106bcafd3312c9e08b4867
A-Team <ateam@pluralsight.com>
2020-01-16 14:56:59 -0500

    Add GET api
    Co-Authored-By: m-labbe <m-labbe@users.noreply.github.com>
    Co-Authored-By: willmcmain <willmcmain@users.noreply.github.com>
    Co-Authored-By: Vishal Ramesh <vishalramesh50@users.noreply.github.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,33 @@
 import 'reflect-metadata'
 import { createConnection, Connection } from 'typeorm'
 import express from 'express'
+import { TodoItem } from './entity/TodoItem'
 
 const app = express()
-createConnection().then((connection) => {
-  if (connection) {
-    app.get('/', (req, res) => {
-      res.send('I love it when a plan comes together!')
-    })
-  }
-})
+createConnection()
+  .then((connection) => {
+    if (connection) {
+      app.get('/', (req, res) => {
+        res.send('I love it when a plan comes together!')
+      })
+
+      app.get('/todo/', async (req, res) => {
+        const todos = await connection
+          .getRepository(TodoItem)
+          .createQueryBuilder('ti')
+          .orderBy('ti.id', 'ASC')
+          .getMany()
+        console.log(todos)
+
+        res.json({
+          status: 'success',
+          data: todos,
+        })
+      })
+    }
+  })
+  .catch((e) => {
+    console.error('Could not connect to database', e)
+  })
 
 export default app
